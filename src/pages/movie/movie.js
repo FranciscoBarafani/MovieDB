edb77c9ff3e293acc20f99733401df5739414ca2
Francisco  Barafani <franciscobarafani@Franciscos-MacBook-Pro.local>
2020-02-01 15:12:34 -0300

    Added Trailer Player

--- src/pages/movie/movie.js
+++ src/pages/movie/movie.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./movie.scss";
 import { Row, Col, Button } from "antd";
 //USE PARAMS ES UN HOOK
@@ -7,6 +7,8 @@ import moment from "moment";
 import useFetch from "../../hooks/useFetch";
 import { URL_API, API } from "../../utils/constants";
 import Loading from "../../components/Loading";
+import ModalVideo from "../../components/ModalVideo";
+import { render } from "@testing-library/react";
 
 export default function Movie() {
   const { id } = useParams();
@@ -23,7 +25,7 @@ export default function Movie() {
 
 function RenderMovie(props) {
   const {
-    movieInfo: { title, backdrop_path }
+    movieInfo: { title, backdrop_path, poster_path }
   } = props;
   const backDropPath = `https://image.tmdb.org/t/p/original${backdrop_path}`;
 
@@ -34,9 +36,73 @@ function RenderMovie(props) {
     >
       <div className="movie__dark" />
       <Row>
-        <Col span={8} offset={3} className="movie__poster"></Col>
-        <Col span={10} className="movie__info"></Col>
+        <Col span={8} offset={3} className="movie__poster">
+          <PosterMovie image={poster_path} />
+        </Col>
+        <Col span={10} className="movie__info">
+          <MovieInfo movieInfo={props.movieInfo} />
+        </Col>
       </Row>
     </div>
+  );
+}
+
+function PosterMovie(props) {
+  const { image } = props;
+  const posterPath = `https://image.tmdb.org/t/p/original${image}`;
+  return <div style={{ backgroundImage: `url('${posterPath}')` }}></div>;
+}
+
+function MovieInfo(props) {
+  const {
+    movieInfo: { title, id, release_date, overview, genres }
+  } = props;
+  const [isVisibleModal, setIsVisibleModal] = useState(false);
+  const videoMovie = useFetch(
+    `${URL_API}/movie/${id}/videos?api_key=${API}&language=es-ES`
+  );
+  const openModal = () => setIsVisibleModal(true);
+  const closeModal = () => setIsVisibleModal(false);
+
+  const renderVideo = () => {
+    if (videoMovie.result) {
+      if (videoMovie.result.results.length > 0) {
+        return (
+          <>
+            <Button icon="play-circle" onClick={openModal}>
+              Ver Trailer
+            </Button>
+            <ModalVideo
+              videoKey={videoMovie.result.results[0].key}
+              videoPlatform={videoMovie.result.results[0].site}
+              isOpen={isVisibleModal}
+              close={closeModal}
+            />
+          </>
+        );
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="movie__info-header">
+        <h1>
+          {title}
+          <span>{moment(release_date, "YYYY-MM-DD").format("YYYY")}</span>
+        </h1>
+        {renderVideo()}
+      </div>
+      <div className="movie__info-content">
+        <h3>General</h3>
+        <p>{overview}</p>
+        <h3>Generos</h3>
+        <ul>
+          {genres.map(gender => (
+            <li key={gender.id}>{gender.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
