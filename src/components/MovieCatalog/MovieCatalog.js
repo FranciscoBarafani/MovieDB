7d529a537cfb61a23b3b6fbca1882156e6c86bfc
Francisco  Barafani <franciscobarafani@Franciscos-MacBook-Pro.local>
2020-02-02 20:31:49 -0300

    Commit

---
+++ src/components/MovieCatalog/MovieCatalog.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Col, Card, Icon } from "antd";
+import "./MovieCatalog.scss";
+import { Link } from "react-router-dom";
+
+export default function MovieCatalog() {
+  const {
+    movies: { results }
+  } = props;
+
+  return results.map(movie => {
+    <Col key={movie.id} xs={4} className="movie-catalog">
+      <MovieCard movie={movie} />
+    </Col>;
+  });
+}
+
+function MovieCard(props) {
+  return "Movie Card";
+}
