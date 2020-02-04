119abc96036ff70b9d21e914f93179dbe024859c
Francisco  Barafani <franciscobarafani@Franciscos-MacBook-Pro.local>
2020-02-04 20:57:33 -0300

    New Movies

--- src/components/MovieCatalog/MovieCatalog.js
+++ src/components/MovieCatalog/MovieCatalog.js
@@ -16,5 +16,21 @@ export default function MovieCatalog(props) {
 }
 
 function MovieCard(props) {
-  return "Movie Card";
+  const {
+    movie: { id, title, poster_path }
+  } = props;
+  const { Meta } = Card;
+  const posterPath = `https://image.tmdb.org/t/p/original/${poster_path}`;
+  return (
+    <Link to={`/movie/${id}`}>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt={title} src={posterPath} />}
+        actions={[<Icon type="eye" key="eye" />]}
+      >
+        <Meta title={title} />
+      </Card>
+    </Link>
+  );
 }
--- src/pages/new-movies.js
+++ src/pages/new-movies.js
@@ -12,7 +12,7 @@ export default function NewMovies() {
   useEffect(() => {
     (async () => {
       const response = await fetch(
-        `${URL_API}/movie/now-playing?api_key=${API}&language=es-ES&page=${page}`
+        `${URL_API}/movie/now_playing?api_key=${API}&language=es-ES&page=${page}`
       );
       const movies = await response.json();
       setMovieList(movies);
@@ -21,21 +21,23 @@ export default function NewMovies() {
 
   return (
     <Row>
-      <Col span="24" style={{ textAlign: "center", marginTop: 25 }}>
+      <Col span={24} style={{ textAlign: "center", marginTop: 25 }}>
         <h1 style={{ fontSize: 35, fontWeight: "bold" }}>
           Ultimos Lanzamientos
         </h1>
       </Col>
       {movieList.results ? (
-        <Col span="24">
+        <Col span={24}>
           <MovieCatalog movies={movieList} />
         </Col>
       ) : (
-        <Col span="24">
+        <Col span={24}>
           <Loading />
         </Col>
       )}
-      <Col span="24"></Col>
+      <Col span={24}>
+        <Footer />
+      </Col>
     </Row>
   );
 }
